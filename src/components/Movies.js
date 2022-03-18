bafe3d543730915a1e1caa91c976e61284b7dcbf
Francisco Martinez <frankmartinez4050@gmail.com>
2022-03-18 16:31:24 -0500

    Completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <>
+    <h1>Movies Page</h1>
+      {movies.map((movie) => {
+        return (
+              <div key={movie.title}>
+                <h2>{movie.title}</h2>
+                <p>Runtime: {movie.time} min.</p>
+                <ul>
+                  {movie.genres.map((genre) => {
+                    return <li key={genre}>{genre}</li>
+                  })}
+                </ul>
+              </div>
+        );
+      })}
+    </>
+    );
 }
 
 export default Movies;
